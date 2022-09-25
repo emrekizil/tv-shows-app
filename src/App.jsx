f03cb88b0f237ca41ddb1313644f008251a28e30
emrekizil <95411289+emrekizil@users.noreply.github.com>
2022-09-25 21:05:30 +0300

    let's go

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-import { useState } from 'react'
 import './index.css'
-import React, { StrictMode } from "react";
+import React from "react";
 import { BrowserRouter, Routes, Route, Link, Router } from "react-router-dom";
 import Form from './components/Form'
 import Details from './components/Details';
